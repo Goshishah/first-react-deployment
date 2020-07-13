fbfe8be5947e00bf10948b76c37c8adb6f0936ac
Shujaat Ali <shujaat.mcs@gmail.com>
2020-07-13 10:19:10 +0500

    Updates

---
+++ precache-manifest.4e5e83d864f166b7147065569c72747e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f7284fb72c96cab442b7923ee6cc13",
+    "url": "/Goshishah/first-react-deployment/index.html"
+  },
+  {
+    "revision": "d45a3a34d26e0548ca7f",
+    "url": "/Goshishah/first-react-deployment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ed5f05ea273c29d7635",
+    "url": "/Goshishah/first-react-deployment/static/js/2.8a22ffe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Goshishah/first-react-deployment/static/js/2.8a22ffe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45a3a34d26e0548ca7f",
+    "url": "/Goshishah/first-react-deployment/static/js/main.1f7e25eb.chunk.js"
+  },
+  {
+    "revision": "1d54d47eb94c87d7f439",
+    "url": "/Goshishah/first-react-deployment/static/js/runtime-main.1a0d2932.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Goshishah/first-react-deployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
